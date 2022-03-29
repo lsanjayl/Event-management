97214b1472302e978443a505fcbbdad8ef37afe9
Charanreddyputhalapattu <75439180+Charanreddyputhalapattu@users.noreply.github.com>
2022-03-29 14:30:46 +0530

    Charan

--- Event/event/src/App.js
+++ Event/event/src/App.js
@@ -2,6 +2,7 @@
 import Dashboard from "./components/dashboard/dashboard"
 // import { Route, Link, Routes, BrowserRouter as Router } from 'react-router-dom'
 import Login from "./components/login/login"
+import Forms from "./components/forms/forms"
 import { useState } from "react";
 function App() {
 
@@ -31,7 +32,8 @@ function App() {
 
   return (
     // <Login/>
-    <Dashboard/>
+    //<Dashboard/>
+    <Forms/>
     // <Router>
     //   <Routes>
     //     <Route path="/" element={<Dashboard blogs={ blogs } />} />
