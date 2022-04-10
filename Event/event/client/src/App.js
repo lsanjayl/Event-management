0a72661867f02fa9901e1a543bea80ce52ce1c6c
sanjay <84283520+lsanjayl@users.noreply.github.com>
2022-04-10 19:30:39 +0530

    2nd commit 10/4/22

--- Event/event/client/src/App.js
+++ Event/event/client/src/App.js
@@ -6,32 +6,7 @@ import ProtectedRoute from './services/ProtectedRoute'
 import { useState,useEffect } from "react";
 import { UserAuthContextProvider } from "./services/authservice";
 function App() {
-<<<<<<< HEAD
-=======
-  const[list,setList]=useState([])
-  const [Title,setTitle]=useState("")
-  const [Theme,setTheme]=useState("")
-  const [Date,setDate]=useState("")
-  const [Duration,setDuration]=useState("")
-  
-useEffect(()=>{
-  Axios.get("http://localhost:3001/forms").then((response)=>{
-    setList(response.data);
-  });
-  
-}, []);
-const createUser=()=>{
-  Axios.post("http://localhost:3001/createuser",{
-    Title,
-    Theme,
-    Date,
-    Duration,  
-  }).then((response)=>{
-    setList([...list,{Title,Theme,Date,Duration}])
-})
-}
 
->>>>>>> 879d4996f4a9d69e4bcbdab698a375b21e697108
   return (
     <UserAuthContextProvider>
       <Routes>
